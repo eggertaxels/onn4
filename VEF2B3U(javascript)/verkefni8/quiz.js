16205d672924bcecff0071d1118a41b2ebf85dec
Eggert Geir Axelsson <eggertgeir@gmail.com>
2017-03-08 01:12:16 +0000

    Add files via upload

---
+++ VEF2B3U(javascript)/verkefni8/quiz.js
@@ -0,0 +1,49 @@
+
+	//Dom element
+	let elbirtaspurningu = document.getElementById('myDiv'); 	
+
+//smiÃ°ur fyrir spurningu
+	function Spurning(spurning, moguleikar, svar) {
+		this.spurning = spurning; 					/* Spurning (strengur) */
+		this.moguleikar = moguleikar; 					/* fylki meÃ° svarmÃ¶guleikum */
+		this.svar = svar; 		/* RÃ©tt svar (strengur) */
+	}
+
+// gogn (fylki af objectum) 
+	let spurningar = [
+			new Spurning("hvad hefur fjorar lappir og geltir", ["hundur","kottur","svin","belja"],"hundur"),
+		];
+
+// Template
+	Spurning.prototype.getTemplate = function(){
+	 	let tmplmoguleikar = "";
+	 	for(let i = 0; i < this.moguleikar.length; i++) {
+	 	if(this.moguleikar[i] === this.svar)
+            {
+                tmplmoguleikar += "<div id='rett'>" +  this.moguleikar[i] + "</div>";
+            }
+            else{
+	 		    tmplmoguleikar += "<div id='id" + i + "'>" +  this.moguleikar[i] + "</div>";
+            }
+	 	}
+	 	return "<h1>" + this.spurning + "</h1>" + tmplmoguleikar;
+	 		
+	};	
+
+		//birta spurningu
+		elbirtaspurningu.innerHTML = spurningar[0].getTemplate();
+		var elRettsvar = document.getElementById('rett');
+/*document.getElementById("id2").onclick = function() {litur3()};
+        function litur3() {
+    document.getElementById("id2").style.backgroundColor = "red";
+}*/
+elbirtaspurningu.addEventListener("click",function(e) {
+	// e.target was the clicked element
+	if(e.target === elRettsvar)
+		{
+			e.target.style.backgroundColor = "green";
+		}
+	else{
+		e.target.style.backgroundColor = "red";
+	}  
+}, false);
