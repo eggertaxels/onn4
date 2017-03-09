e1294a081ed82b995429fbd6542dde02f9ec1fb1
Eggert Geir Axelsson <eggertgeir@gmail.com>
2017-03-09 10:34:27 +0000

    Add files via upload

--- VEF2B3U(javascript)/verkefni8/quiz.js
+++ VEF2B3U(javascript)/verkefni8/quiz.js
@@ -23,16 +23,18 @@
                 tmplmoguleikar += "<div id='rett'>" +  this.moguleikar[i] + "</div>";
             }
             else{
-	 		    tmplmoguleikar += "<div id='id" + i + "'>" +  this.moguleikar[i] + "</div>";
+	 		    tmplmoguleikar += "<div id='id'" + i + "'>" +  this.moguleikar[i] + "</div>";
             }
 	 	}
-	 	return "<h1>" + this.spurning + "</h1>" + tmplmoguleikar;
+	 	return "<h1 id='text'>" + this.spurning + "</h1>" + tmplmoguleikar;
 	 		
 	};	
 
 		//birta spurningu
 		elbirtaspurningu.innerHTML = spurningar[0].getTemplate();
 		var elRettsvar = document.getElementById('rett');
+        var bigDiv = document.getElementById('myDiv');
+        var bigText = document.getElementById('text');
 /*document.getElementById("id2").onclick = function() {litur3()};
         function litur3() {
     document.getElementById("id2").style.backgroundColor = "red";
@@ -42,6 +44,10 @@ elbirtaspurningu.addEventListener("click",function(e) {
 	if(e.target === elRettsvar)
 		{
 			e.target.style.backgroundColor = "green";
+		}
+    else if(e.target === bigDiv || e.target === bigText)
+		{
+			e.target.style.backgroundColor = "none";
 		}
 	else{
 		e.target.style.backgroundColor = "red";
