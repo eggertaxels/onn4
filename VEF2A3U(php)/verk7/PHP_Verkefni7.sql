c600097162a83343d085787a94777077881535da
Eggert Geir Axelsson <eggertgeir@gmail.com>
2017-04-03 01:30:51 +0000

    Add files via upload

---
+++ VEF2A3U(php)/verk7/PHP_Verkefni7.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 03, 2017 at 01:29 AM
+-- Server version: 5.7.17-0ubuntu0.16.04.1
+-- PHP Version: 7.0.15-0ubuntu0.16.04.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `PHP_Verkefni7`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `pass` varchar(25) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `pass`, `email`) VALUES
+(1, 'Eggert', 'test', 'eggertgeir@gmail.com'),
+(2, 'Matti', 'test', 'snefill@gmail.com'),
+(3, 'Gorti', 'test', 'gortigeir@gmail.com');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- VEF2A3U(php)/verk7/breytauppl.php
+++ VEF2A3U(php)/verk7/breytauppl.php
@@ -4,12 +4,13 @@
               session_start();
       $new = $_POST['newname'];
       $pass = $_POST['pass_ga'];
+			$mail = $_SESSION['email'];
     try {
         $sql = "UPDATE users
                 SET name = :newname
-                WHERE pass = :pass;";
+                WHERE pass = :pass AND email = :mail";
         $query = $pdo->prepare($sql);
-        $query->execute( array( ':newname'=>$new, ':pass'=>$pass ) );
+        $query->execute( array( ':newname'=>$new, ':pass'=>$pass, ':mail'=>$mail ) );
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (Exception $e) {
 		echo "Ekki tókst að skrá gögnin". "<br>" . $e->getMessage();
